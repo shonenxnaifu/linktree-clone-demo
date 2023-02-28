ea8bf93da6e6756e60b435ae38165afc526b11f6
Pawitra Warda <pawitrawarda@gmail.com>
2023-02-28 14:13:11 +0700

    initialize project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true
+  },
   reactStrictMode: true,
+  images: {
+    domains: ['avatars.githubusercontent.com']
+  }
 }
 
 module.exports = nextConfig
